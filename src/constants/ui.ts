2ee1eae405805ff1e84061a33de47df90d8abfee
barretodanny <dannybarreto22@gmail.com>
2024-01-08 12:07:28 -0500

    UIScreenState and UIModalState enum constants

---
+++ src/constants/ui.ts
@@ -0,0 +1,12 @@
+export enum UIScreenState {
+  SETUP_GAME = "SETUP_GAME",
+  COUNTDOWN = "COUNTDOWN",
+  IN_GAME = "IN_GAME",
+  POST_GAME = "POST_GAME",
+}
+
+export enum UIModalState {
+  NONE = "NONE",
+  SETTINGS = "SETTINGS",
+  SCORES = "SCORES",
+}
