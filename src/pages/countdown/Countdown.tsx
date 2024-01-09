f62090074c5bb5e6cbb3362825c124ea197a7b67
barretodanny <dannybarreto22@gmail.com>
2024-01-09 09:37:36 -0500

    countdown before starting game

--- src/pages/countdown/Countdown.tsx
+++ src/pages/countdown/Countdown.tsx
@@ -1,12 +1,32 @@
-import { useAppDispatch } from "../../app/hooks";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { selectCountdownTimer } from "../../features/settings/settings-slice";
 import { showGameScreen } from "../../features/ui/ui-slice";
 
 function Countdown() {
   const dispatch = useAppDispatch();
+  const countdownTimer = useAppSelector(selectCountdownTimer);
+  const [timer, setTimer] = useState(countdownTimer);
+
+  useEffect(() => {
+    const itv = setInterval(() => {
+      setTimer((prev) => prev - 1);
+
+      // dispatched after 'GO!' is displayed
+      if (timer <= 0) {
+        dispatch(showGameScreen());
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(itv);
+    };
+  }, [timer]);
 
   return (
     <div>
       <h2>Countdown</h2>
+      <h4>{timer === 0 ? "GO!" : timer}</h4>
       <button onClick={() => dispatch(showGameScreen())}>Next</button>
     </div>
   );
