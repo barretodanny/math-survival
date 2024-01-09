a27f8e987455cd589d0d18b7d86486fed1ca6beb
barretodanny <dannybarreto22@gmail.com>
2024-01-08 21:11:14 -0500

    countdown timer constants for max/min/default values, fetch settings from local storage when initializing state, save changes to local storage when updating the state

--- src/features/settings/settings-slice.ts
+++ src/features/settings/settings-slice.ts
@@ -1,5 +1,10 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 import { RootState } from "../../app/store"
+import {
+  COUNTDOWN_TIMER_MAX,
+  COUNTDOWN_TIMER_MIN,
+  DEFAULT_COUNTDOWN_TIMER,
+} from "../../constants/settings"
 
 export interface SettingsOptions {
   darkMode: boolean
@@ -8,12 +13,47 @@ export interface SettingsOptions {
   countdownTimer: number
 }
 
-const initialState: SettingsOptions = {
-  darkMode: false, // TODO: fetch from localstorage
-  defaultUsername: "username", // TODO: fetch from localstorage
-  autoSave: false, // TODO: fetch from localstorage
-  countdownTimer: 3, // TODO: fetch from localstorage
+function parseCountdownTimer(countdownTimer: string | null) {
+  // if no saved value, set to default value
+  if (!countdownTimer) {
+    return DEFAULT_COUNTDOWN_TIMER
+  }
+
+  const n = parseInt(countdownTimer)
+
+  // if not a number, set to default value
+  if (isNaN(n)) {
+    return DEFAULT_COUNTDOWN_TIMER
+  }
+
+  // check if n is greater or if n is less than the max/min
+  if (n > COUNTDOWN_TIMER_MAX) {
+    return COUNTDOWN_TIMER_MAX
+  } else if (n < COUNTDOWN_TIMER_MIN) {
+    return COUNTDOWN_TIMER_MIN
+  } else {
+    return n
+  }
 }
+
+function initializeState() {
+  // check local storage for saved settings
+  const darkMode = localStorage.getItem("darkMode")
+  const defaultUsername = localStorage.getItem("defaultUsername")
+  const autoSave = localStorage.getItem("autoSave")
+  const countdownTimer = localStorage.getItem("countdownTimer")
+
+  const state = {
+    darkMode: darkMode ? (darkMode === "true" ? true : false) : false,
+    defaultUsername: defaultUsername ? defaultUsername : "username",
+    autoSave: autoSave ? (autoSave === "true" ? true : false) : false,
+    countdownTimer: parseCountdownTimer(countdownTimer),
+  }
+
+  return state
+}
+
+const initialState: SettingsOptions = initializeState()
 
 export const settingsSlice = createSlice({
   name: "settings",
@@ -21,24 +61,29 @@ export const settingsSlice = createSlice({
   reducers: {
     toggleDarkMode: (state) => {
       state.darkMode = !state.darkMode
+      localStorage.setItem("darkMode", state.darkMode ? "true" : "false")
     },
     setDefaultUsername: (state, action: PayloadAction<string>) => {
       state.defaultUsername = action.payload
+      localStorage.setItem("defaultUsername", state.defaultUsername)
     },
     toggleAutoSave: (state) => {
       state.autoSave = !state.autoSave
+      localStorage.setItem("autoSave", state.autoSave ? "true" : "false")
     },
     incrementCountdownTimer: (state) => {
       // maximum countdown timer is 5
-      if (state.countdownTimer >= 5) return
+      if (state.countdownTimer >= COUNTDOWN_TIMER_MAX) return
 
       state.countdownTimer += 1
+      localStorage.setItem("countdownTimer", String(state.countdownTimer))
     },
     decrementCountdownTimer: (state) => {
       // minimum countdown timer is 2
-      if (state.countdownTimer <= 2) return
+      if (state.countdownTimer <= COUNTDOWN_TIMER_MIN) return
 
       state.countdownTimer -= 1
+      localStorage.setItem("countdownTimer", String(state.countdownTimer))
     },
   },
 })
