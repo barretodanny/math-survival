1cac5af8221a2b7a8f20807dde27e48636af5d92
barretodanny <dannybarreto22@gmail.com>
2024-01-10 11:30:22 -0500

    post game screen, display final game stats, also save score flow

--- src/pages/postgame/PostGame.tsx
+++ src/pages/postgame/PostGame.tsx
@@ -1,13 +1,64 @@
-import { useAppDispatch } from "../../app/hooks";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { selectGameScore } from "../../features/game/game-slice";
+import {
+  selectAutoSaveSetting,
+  selectDefaultUsername,
+} from "../../features/settings/settings-slice";
 import { showSetupScreen } from "../../features/ui/ui-slice";
 
 function PostGame() {
   const dispatch = useAppDispatch();
+  const score = useAppSelector(selectGameScore);
+  const autoSave = useAppSelector(selectAutoSaveSetting);
+  const defaultUsername = useAppSelector(selectDefaultUsername);
+  const [username, setUsername] = useState(defaultUsername);
+  const [saved, setSaved] = useState(false);
 
   return (
     <div>
       <h2>PostGame</h2>
-      <button onClick={() => dispatch(showSetupScreen())}>Next</button>
+      <h4>You survived {score + 60} seconds</h4>
+      <h4>Final score: {score}</h4>
+      {autoSave ? (
+        <>
+          {/* TODO implement auto save */}
+          <p>
+            Score has been automatically saved under username {defaultUsername}
+          </p>
+          <p>You can change this in the settings.</p>
+        </>
+      ) : (
+        <>
+          {saved ? (
+            <>
+              <p>Score saved</p>
+            </>
+          ) : (
+            <>
+              <p>Would you like to save this score?</p>
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <button
+                onClick={() => {
+                  console.log(
+                    `Saving score of ${score} under username ${username}`
+                  );
+                  setSaved(true);
+                }}
+              >
+                Save
+              </button>
+            </>
+          )}
+        </>
+      )}
+      <br />
+      <button onClick={() => dispatch(showSetupScreen())}>Play Again</button>
     </div>
   );
 }
