2a810b8dad7df3f9713d3ac9eb1f70b75c268f86
barretodanny <dannybarreto22@gmail.com>
2024-01-12 15:56:13 -0500

    Question component

---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,39 @@
+import { useAppDispatch } from "../../app/hooks";
+import { updateUserAnswer } from "../../features/game/game-slice";
+import { QuestionType } from "../../types/game";
+
+import styles from "./Question.module.css";
+
+interface QuestionProps {
+  question: QuestionType | undefined;
+  previous?: boolean;
+  current?: boolean;
+  userAnswer?: string;
+}
+
+function Question({ question, previous, current, userAnswer }: QuestionProps) {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div
+      className={`${styles.container} ${
+        current ? styles.current : previous ? styles.previous : styles.next
+      }`}
+    >
+      <span className={styles.question}>
+        {question?.n1} {question?.sign} {question?.n2}
+      </span>
+      {current && (
+        <input
+          type="text"
+          value={userAnswer}
+          onChange={(e) => dispatch(updateUserAnswer(e.target.value))}
+          autoFocus
+          className={styles.answerInput}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Question;
