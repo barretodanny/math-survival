e2a602fc8e0f9b83f8b2c8aa5b6f287edb14f076
barretodanny <dannybarreto22@gmail.com>
2024-01-12 15:56:39 -0500

    game screen styling

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -6,16 +6,16 @@ import {
   selectGameMax,
   selectGameMin,
   selectGameNextQuestions,
-  selectGamePreviousQuestion,
   selectGameScore,
   selectGameTimer,
   selectGameUserAnswer,
-  updateUserAnswer,
 } from "../../features/game/game-slice";
-import {
-  showPostGameScreen,
-  showSetupScreen,
-} from "../../features/ui/ui-slice";
+import { showPostGameScreen } from "../../features/ui/ui-slice";
+
+import GameInfoBar from "../../components/GameInfoBar/GameInfoBar";
+import QuestionsSlider from "../../components/QuestionsSlider/QuestionsSlider";
+
+import styles from "./Game.module.css";
 
 function Game() {
   const dispatch = useAppDispatch();
@@ -23,7 +23,6 @@ function Game() {
   const score = useAppSelector(selectGameScore);
   const min = useAppSelector(selectGameMin);
   const max = useAppSelector(selectGameMax);
-  const previousQuestion = useAppSelector(selectGamePreviousQuestion);
   const currentQuestion = useAppSelector(selectGameCurrentQuestion);
   const userAnswer = useAppSelector(selectGameUserAnswer);
   const nextQuestions = useAppSelector(selectGameNextQuestions);
@@ -46,51 +45,13 @@ function Game() {
   }, [timer]);
 
   return (
-    <div>
-      <h2>Game</h2>
-      <h4>Timer: {timer}</h4>
-      <h4>Score: {score}</h4>
-      <h4>Min: {min}</h4>
-      <h4>Max: {max}</h4>
-      <div>
-        <h5>Prev Question</h5>
-        <span>
-          {previousQuestion?.n1} {previousQuestion?.sign} {previousQuestion?.n2}
-        </span>
-      </div>
-      <div>
-        <h5>Current Question</h5>
-        <span>
-          {currentQuestion?.n1} {currentQuestion?.sign} {currentQuestion?.n2}
-        </span>
-        <input
-          type="text"
-          value={userAnswer ? userAnswer : ""}
-          onChange={(e) => dispatch(updateUserAnswer(e.target.value))}
-          autoFocus
-        />
-      </div>
-      <div>
-        <h5>Next Question</h5>
-        <span>
-          {nextQuestions![0].n1} {nextQuestions![0].sign} {nextQuestions![0].n2}
-        </span>
-      </div>
-      <div>
-        <h5>Next Next Question</h5>
-        <span>
-          {nextQuestions![1].n1} {nextQuestions![1].sign} {nextQuestions![1].n2}
-        </span>
-      </div>
-      <div>
-        <h5>Next Next Next Question</h5>
-        <span>
-          {nextQuestions![2].n1} {nextQuestions![2].sign} {nextQuestions![2].n2}
-        </span>
-      </div>
-      <button onClick={() => dispatch(showSetupScreen())}>Quit</button>
-      <br />
-      <button onClick={() => dispatch(showPostGameScreen())}>Next</button>
+    <div className={styles.container}>
+      <GameInfoBar timer={timer} score={score} min={min} max={max} />
+      <QuestionsSlider
+        currentQuestion={currentQuestion}
+        nextQuestions={nextQuestions}
+        userAnswer={userAnswer}
+      />
     </div>
   );
 }
