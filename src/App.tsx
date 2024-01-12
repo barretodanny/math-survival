bd7196e990833d255be8da9fd6b86329d0504135
barretodanny <dannybarreto22@gmail.com>
2024-01-12 13:11:05 -0500

    app styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { useAppSelector } from "./app/hooks";
 import { selectUIScreen } from "./features/ui/ui-slice";
 import { determineUIScreenContent } from "./features/ui/ui";
+import { selectDarkModeSetting } from "./features/settings/settings-slice";
+
+import styles from "./App.module.css";
 
 function App() {
   const screen = useAppSelector(selectUIScreen);
+  const darkMode = useAppSelector(selectDarkModeSetting);
 
   return (
-    <div>
-      <h2>Math Survival</h2>
+    <div
+      className={`${styles.container} ${
+        darkMode ? `${styles.bgDark}` : `${styles.bgLight}`
+      }`}
+    >
+      <h2
+        className={`${styles.heading} ${
+          darkMode ? `${styles.headingDark}` : `${styles.headingLight}`
+        }`}
+      >
+        Mad Minute Survival
+      </h2>
       {determineUIScreenContent(screen)}
     </div>
   );
