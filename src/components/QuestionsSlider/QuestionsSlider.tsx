bc09924706cd036c663258fbcb04ebeff17dbf23
barretodanny <dannybarreto22@gmail.com>
2024-01-12 15:56:26 -0500

    Questions Slider component

---
+++ src/components/QuestionsSlider/QuestionsSlider.tsx
@@ -0,0 +1,38 @@
+import { useAppSelector } from "../../app/hooks";
+import { selectGameScore } from "../../features/game/game-slice";
+import { QuestionType } from "../../types/game";
+
+import Question from "../Question/Question";
+
+import styles from "./QuestionsSlider.module.css";
+
+interface QuestionsSliderProps {
+  currentQuestion: QuestionType | undefined;
+  nextQuestions: [QuestionType, QuestionType, QuestionType] | undefined;
+  userAnswer: string | undefined;
+}
+
+function QuestionsSlider({
+  currentQuestion,
+  nextQuestions,
+  userAnswer,
+}: QuestionsSliderProps) {
+  const score = useAppSelector(selectGameScore);
+
+  return (
+    <div className={styles.container}>
+      {/* <Question key={score} question={previousQuestion} previous={true} /> */}
+      <Question
+        key={score + 1}
+        question={currentQuestion}
+        current={true}
+        userAnswer={userAnswer}
+      />
+      <Question key={score + 2} question={nextQuestions![0]} />
+      <Question key={score + 3} question={nextQuestions![1]} />
+      <Question key={score + 4} question={nextQuestions![2]} />
+    </div>
+  );
+}
+
+export default QuestionsSlider;
