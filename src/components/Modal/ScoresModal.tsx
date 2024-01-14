cc13cd63752fd8821b6cf5413c0ab00be81712cb
barretodanny <dannybarreto22@gmail.com>
2024-01-14 15:43:15 -0500

    modal styling

--- src/components/Modal/ScoresModal.tsx
+++ src/components/Modal/ScoresModal.tsx
@@ -4,12 +4,17 @@ import {
   selectDifficultyOption,
   selectMathModeOption,
 } from "../../features/game/game-slice";
+import { selectDarkModeSetting } from "../../features/settings/settings-slice";
 import Modal from "./Modal";
+import ScoresTable from "../ScoresTable/ScoresTable";
 import { DifficultyOptions, MathModeOptions } from "../../constants/game";
 import { generateGameString, parseScores } from "../../utils/scores";
 import { Score } from "../../types/scores";
 
+import styles from "./Modal.module.css";
+
 function ScoresModal() {
+  const darkMode = useAppSelector(selectDarkModeSetting);
   const initialMathMode = useAppSelector(selectMathModeOption);
   const initialDifficulty = useAppSelector(selectDifficultyOption);
   const [mathMode, setMathMode] = useState(initialMathMode);
@@ -21,53 +26,117 @@ function ScoresModal() {
 
   return (
     <Modal>
-      <span>Scores</span>
-      <p>Game string: {gameString}</p>
-      <div>
-        <button onClick={() => setMathMode(MathModeOptions.ADDITION)}>
-          Addition
-        </button>
-        <button onClick={() => setMathMode(MathModeOptions.SUBTRACTION)}>
-          Subtraction
-        </button>
-        <button onClick={() => setMathMode(MathModeOptions.MULTIPLICATION)}>
-          Multiplication
-        </button>
-      </div>
-      <br />
-      <div>
-        <button onClick={() => setDifficulty(DifficultyOptions.EASY)}>
-          Easy
-        </button>
-        <button onClick={() => setDifficulty(DifficultyOptions.NORMAL)}>
-          Normal
-        </button>
-        <button onClick={() => setDifficulty(DifficultyOptions.HARD)}>
-          Hard
-        </button>
-      </div>
-      <p>{mathMode}</p>
-      <p>{difficulty}</p>
+      <div className={styles.content}>
+        <h3 className={`${styles.scoresModalHeading} ${styles.centerText}`}>
+          Scores
+        </h3>
+        <div className={styles.scoresSection}>
+          <h4 className={`${styles.sectionHeading} ${styles.centerText}`}>
+            Math Mode
+          </h4>
+          <div className={styles.btnContainer}>
+            <button
+              onClick={() => setMathMode(MathModeOptions.ADDITION)}
+              className={`${styles.btn} 
+            ${
+              mathMode === MathModeOptions.ADDITION
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Addition
+            </button>
+            <button
+              onClick={() => setMathMode(MathModeOptions.SUBTRACTION)}
+              className={`${styles.btn} 
+            ${
+              mathMode === MathModeOptions.SUBTRACTION
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Subtraction
+            </button>
+            <button
+              onClick={() => setMathMode(MathModeOptions.MULTIPLICATION)}
+              className={`${styles.btn} 
+            ${
+              mathMode === MathModeOptions.MULTIPLICATION
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Multiplication
+            </button>
+          </div>
+        </div>
+        <div className={styles.scoresSection}>
+          <h4 className={`${styles.sectionHeading} ${styles.centerText}`}>
+            Difficulty
+          </h4>
+          <div className={styles.btnContainer}>
+            <button
+              onClick={() => setDifficulty(DifficultyOptions.EASY)}
+              className={`${styles.btn} 
+            ${
+              difficulty === DifficultyOptions.EASY
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Easy
+            </button>
+            <button
+              onClick={() => setDifficulty(DifficultyOptions.NORMAL)}
+              className={`${styles.btn} 
+            ${
+              difficulty === DifficultyOptions.NORMAL
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Normal
+            </button>
+            <button
+              onClick={() => setDifficulty(DifficultyOptions.HARD)}
+              className={`${styles.btn} 
+            ${
+              difficulty === DifficultyOptions.HARD
+                ? darkMode
+                  ? `${styles.selectedBtnDark}`
+                  : `${styles.selectedBtnLight}`
+                : darkMode
+                ? `${styles.btnDark}`
+                : `${styles.btnLight}`
+            }`}
+            >
+              Hard
+            </button>
+          </div>
+        </div>
 
-      {scores.length > 0 ? (
-        <>
-          <h4>List of scores:</h4>
-          {scores.map((score, index) => {
-            return (
-              <div key={index}>
-                <span>
-                  username: {score.username} - score: {score.score} - date:{" "}
-                  {score.date}
-                </span>
-              </div>
-            );
-          })}
-        </>
-      ) : (
-        <>
-          <p>No scores found</p>
-        </>
-      )}
+        <ScoresTable fetchedScores={scores} />
+      </div>
     </Modal>
   );
 }
